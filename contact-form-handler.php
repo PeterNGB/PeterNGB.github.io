d2f33c922d48a7ed2a5b4a358a5b2055a45ff9d0
Peter Neugebauer <38669749+PeterNGB@users.noreply.github.com>
2018-08-12 16:29:20 +0200

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,16 +1,18 @@
 <?php 
 $errors = '';
 $myemail = 'nocoinproject@gmx.net';//<-----Put Your email address here.
-if(empty($_POST['name'])  || 
-   empty($_POST['email']) || 
-   empty($_POST['message']))
+if(empty($_POST['email'])  || 
+   empty($_POST['name']) ||
+   empty($_POST['noaddress']) ||
+   empty($_POST['payid']))
 {
     $errors .= "\n Error: all fields are required";
 }
 
-$name = $_POST['name']; 
 $email_address = $_POST['email']; 
-$message = $_POST['message']; 
+$name = $_POST['name']; 
+$message = $_POST['noaddress'];
+$payid = $_POST['payid']; 
 
 if (!preg_match(
 "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
@@ -24,13 +26,13 @@ if( empty($errors))
 	$to = $myemail; 
 	$email_subject = "Contact form submission: $name";
 	$email_body = "You have received a new message. ".
-	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message"; 
+	" Here are the details:\n Name: $name \n Email: $email_address \n NoAddress: \n $message \n Payment ID: \n $payid"; 
 	
 	$headers = "From: $myemail\n"; 
 	$headers .= "Reply-To: $email_address";
 	
 	mail($to,$email_subject,$email_body,$headers);
 	//redirect to the 'thank you' page
-	header('Location: contact-form-thank-you.html');
+	header('Location: index.html');
 } 
 ?>
